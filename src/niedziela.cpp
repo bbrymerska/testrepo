1eb2c7f4428e0e1f9847b576b162f0b6ce0283fe
bbrymerska <rozumserce@gmail.com>
2018-05-29 22:46:38 +0200

    Initial commit

---
+++ src/niedziela.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : niedziela.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+void findmax() {
+	int input;
+	for (int i = 0; i<20; i++) {
+		cout << "podaj licznbe";
+		cin >> input;
+		if (i) {
+			cout << input;
+		} else {
+			break;
+		}
+	}
+}
+
+int main() {
+	findmax();
+	return 0;
+}
